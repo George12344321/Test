0827f5512fc8de5401252833edee5832176401fd
George Ibrahim Fuad  Alatrash <201706021@NATIVITY.BETHLEHEM.EDU>
2018-05-04 15:20:35 +0300

    main edited

--- src/practice/Course.java
+++ src/practice/Course.java
@@ -1,5 +1,53 @@
 package practice;
 
+import java.util.ArrayList;
+
 public class Course {
+String code;
+String name;
+ArrayList<Double> grades;
+
+
+public  Course() {
+	
+}
+
+
+public Course(String code, String name, ArrayList<Double> grades) {
+	super();
+	this.code = code;
+	this.name = name;
+	this.grades = grades;
+}
+
+
+public String getCode() {
+	return code;
+}
+
+
+public void setCode(String code) {
+	this.code = code;
+}
+
+
+public String getName() {
+	return name;
+}
+
+
+public void setName(String name) {
+	this.name = name;
+}
+
+
+public ArrayList<Double> getGrades() {
+	return grades;
+}
+
+
+public void setGrades(ArrayList<Double> grades) {
+	this.grades = grades;
+}
 
 }
